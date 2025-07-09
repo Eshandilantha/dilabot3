eac46147d01ccf09eaf63296ece44f2e430ce9aa
Eshandilantha <eshansenadeera19@gmail.com>
2025-07-09 18:06:27 +0400

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Eshandilantha/dilabot3/blob/main/images/cd1e54ef-a13c-4061-a436-69953b5b0eb7.jpg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 dilabot Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+BOT_OWNER: '94784398732',  // Replace with the owner's phone number
 
 
 
