7662c1841efca513f2e190fc12f53b48915e8248
Eshandilantha <eshansenadeera19@gmail.com>
2025-07-13 09:51:52 +0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "hloVQAyT#Lz3hAxGZPssYfgcHmV_YZRKEG0_BsBlPex_Bn_hrWmo",
+SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Eshandilantha/dilabot3/blob/main/images/cd1e54ef-a13c-4061-a436-69953b5b0eb7.jpg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 dilabot Is Alive Now😍*",
 BOT_OWNER: '94784398732',  // Replace with the owner's phone number
